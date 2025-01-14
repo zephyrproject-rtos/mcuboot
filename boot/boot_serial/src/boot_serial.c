0c3ecbef9d6c8a0a982fe8181f863dd926e3098a
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2025-01-14 16:42:00 +0000

    boot: bootutil: Refactor some functions to have state
    Refactors some functions so that the state variable is present in it
    
    Note: This PR has been modified from the upstream commit due to
    a merge failure from other changes that are not being brought into
    Zephyr 3.7
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>
    (cherry picked from commit 2fe9cd4b6cf34499fb54c9551e01ca6fb0b3e9f8)

--- boot/boot_serial/src/boot_serial.c
+++ boot/boot_serial/src/boot_serial.c
@@ -310,7 +310,7 @@ bs_list(char *buf, int len)
                         }
 #endif
 
-                        FIH_CALL(bootutil_img_validate, fih_rc, NULL, 0, &hdr,
+                        FIH_CALL(bootutil_img_validate, fih_rc, NULL, &hdr,
                                  fap, tmpbuf, sizeof(tmpbuf), NULL, 0, NULL);
 #if defined(MCUBOOT_ENC_IMAGES) && !defined(MCUBOOT_SINGLE_APPLICATION_SLOT)
                     }
@@ -499,7 +499,7 @@ bs_set(char *buf, int len)
                                  &hdr, tmpbuf, sizeof(tmpbuf));
                     } else {
 #endif
-                        FIH_CALL(bootutil_img_validate, fih_rc, NULL, 0, &hdr,
+                        FIH_CALL(bootutil_img_validate, fih_rc, NULL, &hdr,
                                  fap, tmpbuf, sizeof(tmpbuf), NULL, 0, NULL);
 #ifdef MCUBOOT_ENC_IMAGES
                     }
