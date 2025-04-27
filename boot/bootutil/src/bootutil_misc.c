5a7ee345e77b42d576e9866e9a4781d87c3768ea
Thomas Altenbach <thomas.altenbach@legrand.com>
2025-04-27 19:30:53 +0200

    boot: bootutil: Fix max image size computation for swap-move/swap-offset
    When computing the maximum image size in bootutil_max_image_size for
    swap-move or swap-offset strategy, the computation was using the size of
    the flash area provided as argument and was not taking into account the
    size of the padding sector. This was causing an incorrect size to be
    returned in some cases, for example when the two slots have the same
    size or when the slots haven't the same size but the routine is called
    for the slot containing the padding sector.
    
    For example, let's imagine swap-move is being used on a device having a
    sector size S and two slots of N bytes. This is valid configuration and
    the maximum image size is N - S - T, T being the size of the trailer
    rounded up to the next multiple of S. When calling
    bootutil_max_image_size with either the primary or secondary slot, the
    size N - T is returned, which is incorrect.
    
    This commit fixes the issue by computing always the maximum image using
    the size of the slot containing the padding and substracting the size of
    the padding and of the aligned trailer.
    
    Note: This PR has been modified from the upstream commit due to
    a merge failure from other changes that are not being brought into
    Zephyr 3.7
    
    Signed-off-by: Thomas Altenbach <thomas.altenbach@legrand.com>
    (cherry picked from commit 61d280b9bc87d33cb3c97205edf4532f1365140d)

--- boot/bootutil/src/bootutil_misc.c
+++ boot/bootutil/src/bootutil_misc.c
@@ -412,16 +412,19 @@ uint32_t bootutil_max_image_size(struct boot_loader_state *state, const struct f
 #elif defined(MCUBOOT_SWAP_USING_MOVE)
     (void) state;
 
-    struct flash_sector sector;
-    /* get the last sector offset */
-    int rc = flash_area_get_sector(fap, boot_status_off(fap), &sector);
-    if (rc) {
-        BOOT_LOG_ERR("Unable to determine flash sector of the image trailer");
-        return 0; /* Returning of zero here should cause any check which uses
-                   * this value to fail.
-                   */
-    }
-    return flash_sector_get_off(&sector);
+    /* The slot whose size is used to compute the maximum image size must be the one containing the
+     * padding required for the swap. */
+    const struct flash_area *fap_padded_slot = BOOT_IMG_AREA(state, BOOT_PRIMARY_SLOT);
+    assert(fap_padded_slot != NULL);
+
+    size_t trailer_sz = boot_trailer_sz(BOOT_WRITE_SZ(state));
+    size_t sector_sz = boot_img_sector_size(state, BOOT_PRIMARY_SLOT, 0);
+    size_t padding_sz = sector_sz;
+
+    /* The trailer size needs to be sector-aligned */
+    trailer_sz = ALIGN_UP(trailer_sz, sector_sz);
+
+    return flash_area_get_size(fap_padded_slot) - trailer_sz - padding_sz;
 #elif defined(MCUBOOT_OVERWRITE_ONLY)
     (void) state;
     return boot_swap_info_off(fap);
